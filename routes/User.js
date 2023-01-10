2c7af4fa38e2189d0fddec55a4d21d83e726582f
hidayattresnadi <hidayattresnadi@gmail.com>
2023-01-10 15:21:56 +0700

    login route

--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,7 @@ const UserController = require('../Controller/UserController')
 const router = express.Router()
 
 router.post('/register',UserController.register)
-// router.post('/login',UserController.login)
+router.post('/login',UserController.login)
 // router.post('/googleLogin',UserController.googleLogin)
 // router.get('/showGenshin',Controller.showGenshinCharacter)
 // router.get('/showAnime',Controller.showAnime)
